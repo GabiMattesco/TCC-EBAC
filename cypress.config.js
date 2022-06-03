cfb55511a2bd26db1002379a36e1121412d52c71
Gabriela Mattesco <gabimattesco@gmail.com>
2022-06-03 15:24:27 -0300

    alteracao

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,6 @@ module.exports = defineConfig({
   projectId: '1dzdyi',
   e2e: {
   baseUrl: 'http://lojaebac.ebaconline.art.br/'
-  }
+}
 })
 
--- cypress/e2e/TesteWeb.spec.cy.js
+++ cypress/e2e/TesteWeb.spec.cy.js
@@ -1,8 +1,31 @@
 /// <reference types="cypress" />   
+const perfil = require('../fixtures/perfil.json')
+import enderecopage from '../support/page_objects.js/endereco.page';
+import comandos from '../support/commands'
 
 context('TCC - WEB', () => {
 
     beforeEach(() => {
-        cy.visit('minha-conta/')
-    })
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.fixture('perfil').then((dados) => {
+            cy.login(dados.usuario, dados.senha)
+        });
+        cy.get('.page-title').should('contain', 'Minha conta')
+    });
+
+    it.only('Deve incluir 3 itens no carrinho com sucesso - Usando comandos customizados', () => {
+        cy.preCadastro('Gabriela', 'Mattesco')
+        enderecopage.editarEnderecoFaturamento('Gabriela', 'Mattesco', 'Ebac', 'Brasil', 'Av. Brasil', '100', 'Rio de Janeiro', 'Rio de Janeiro', '21530000', '2177777777', 'gabrielamattesco@msn.com')
+        enderecopage.editarEnderecoEntrega('Gabriela', 'Mattesco', 'Ebac', 'Brasil', 'Av. Brasil', '100', 'Rio de Janeiro', 'Rio de Janeiro', '21530000')
+
+        cy.addProdutos('2', '33', 'Blue', 2),
+        cy.addProdutos('0', 'L', 'Red', 5),
+        cy.addProdutos('1', 'XL', 'Yellow', 2)
+
+        cy.get('.woocommerce-message > .button').click({ force: true }) 
+        cy.get(':nth-child(1) > .product-name > a').should('contain', 'Aether Gym Pant - 33, Blue')
+        cy.get(':nth-child(2) > .product-name > a').should('contain', 'Abominable Hoodie - L, Red')
+        cy.get(':nth-child(3) > .product-name > a').should('contain', 'Aero Daily Fitness Tee - XL, Yellow')
+    });
+    
 })
